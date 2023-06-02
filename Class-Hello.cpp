0c215ca564525b0ca06fb2f6a388ed54ac6bf487
habiba023 <ummehabiba17241724@gmail.com>
2023-06-02 12:33:49 +0500

    Display msg using constructor

---
+++ Class-Hello.cpp
@@ -0,0 +1,29 @@
+/* 
+  **************************
+    Program Name :: Write class display msg on screen using object of class .
+    Program Description :: In this Program , Write Class  that display msg on screen whenever an object of that class is created .
+    "using constructor" 
+    Program Date :: June 02,2023
+    Program Author :: Habiba
+  **************************
+ */  
+#include <iostream> 
+using namespace std;
+
+class Hello
+{
+private:
+int n;
+public:
+Hello()
+{
+ cout<<" Programming easy....\n";
+  
+}
+};
+int main()
+{
+    system ("cls");
+   Hello w,x,y,z ;
+    return 0;
+}
